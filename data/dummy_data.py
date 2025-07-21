701a3bd55f0358eeaafd049ad85b74f81e5a7344
JR2909 <j.r.k290905@gmail.com>
2025-07-21 20:18:44 +0200

    Add files via upload

---
+++ data/dummy_data.py
@@ -0,0 +1,7 @@
+def load_data():
+    pie_data = {"Kategorie": ["A", "B", "C"], "Wert": [30, 50, 20]}
+    bar_data = {"Monat": ["Jän", "Feb", "Mär"], "Umsatz": [100, 150, 130]}
+    column_data1 = {"Produkt": ["X", "Y", "Z"], "Stückzahl": [10, 20, 15]}
+    column_data2 = {"Region": ["Nord", "Süd", "West"], "Zahl": [5, 7, 3]}
+    bar_data_left = {"Faktor": ["F1", "F2", "F3"], "Score": [80, 65, 90]}
+    return pie_data, bar_data, column_data1, column_data2, bar_data_left
